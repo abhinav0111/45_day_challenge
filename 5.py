8fecc7c7f83be01ad22928fd8254bb4b5f9acc81
abhinav0111 <121910302017@gitam.in>
2022-05-06 20:00:52 +0530

    One

--- 5.py
+++ 5.py
@@ -1,6 +1,6 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         k1 = sum(list(set(nums)))
-        n = len(nums)
         n1 = len(list(set(nums)))
+        n = len(nums)
         return (sum(nums)-k1)//(n-n1)
